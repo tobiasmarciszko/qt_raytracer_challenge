6c2a8e576cbed44912f469f785f7e294bcf6602e
Tobias Marciszko <tobias.marciszko@gmail.com>
2019-05-06 11:10:45 +0200

    Got rid of some warnings in the projectile management

--- raytracer/main/raytracer.cpp
+++ raytracer/main/raytracer.cpp
@@ -16,8 +16,8 @@ struct environment {
     Vector wind;
 };
 
-projectile p(Point(0,1,0), Vector(10,100,0).normalize());
-environment e(Vector(0, -0.0098, 0), Vector(0.01, 0, 0));
+static projectile p(Point(0,1,0), Vector(10,100,0).normalize());
+static environment e(Vector(0, -0.0098, 0), Vector(0.01, 0, 0));
 
 raytracer::raytracer() : m_canvas(Canvas(320,240))
 {
@@ -32,25 +32,25 @@ void raytracer::update()
         auto vel = p.velocity + e.gravity + e.wind;
 
         p = projectile(pos, vel);
-        int x = p.position.x();
+        int x = static_cast<int>(p.position.x());
         x = x % 319;
         if (x < 0) x = 0;
 
-        int y = 240 - p.position.y();
+        int y = 240 - static_cast<int>(p.position.y());
         y = y % 239;
         if (y < 0) y = 0;
-        m_canvas.write_pixel(x, y, Color(1.0, 1.0, 1.0));
+        m_canvas.write_pixel(static_cast<unsigned int>(x), static_cast<unsigned int>(y), Color(1.0, 0.5, 1.0));
     }
 
     // Transfer canvas colors to framebuffer, maybe there is no need for an intermediate canvas?
     for (unsigned int i = 0; i < 320; ++i) {
         for (unsigned int j = 0; j < 240; ++j) {
             Color c = m_canvas.pixel_at(i, j);
-            double r = c.red() * 255;
-            double g = c.green() * 255;
-            double b = c.blue() * 255;
+            int r = static_cast<int>(c.red() * 255);
+            int g = static_cast<int>(c.green() * 255);
+            int b = static_cast<int>(c.blue() * 255);
 
-            framebuffer.setPixel(i, j, qRgb(r, g, b));
+            framebuffer.setPixel(static_cast<int>(i), static_cast<int>(j), qRgb(r, g, b));
         }
     }
 }
