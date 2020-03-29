05cbfa961bb5c1b8d3f9af6a11e4128dd953faf4
Tobias Marciszko <tobias.marciszko@gmail.com>
2020-03-29 20:59:34 +0200

    Removed tuple.cpp and vector.cpp and moved the code in the their header files.

--- raytracer/main/tuple.h
+++ raytracer/main/tuple.h
@@ -2,21 +2,58 @@
 #define TUPLE_H
 
 #include <iostream>
+#include "equal.h"
 
 struct Tuple
 {
     Tuple() = default;
-    Tuple(float x, float y, float z, float w);
+    Tuple(float x_, float y_, float z_, float w_) :
+        x(x_),
+        y(y_),
+        z(z_),
+        w(w_)
+    {
+    }
 
-    friend Tuple operator-(const Tuple& t);
-    friend Tuple operator+(const Tuple& lhs, const Tuple& rhs);
-    friend Tuple operator-(const Tuple& lhs, const Tuple& rhs);
-    friend Tuple operator*(const Tuple& t, float multiplier);
-    friend Tuple operator/(const Tuple& t, float divider);
-    friend bool operator==(const Tuple& lhs, const Tuple& rhs);
+    [[nodiscard]] friend Tuple operator-(const Tuple& t) {
+        return {-t.x, -t.y, -t.z, -t.w};
+    }
 
-    [[nodiscard]] bool isPoint() const;
-    [[nodiscard]] bool isVector() const;
+    [[nodiscard]] friend Tuple operator+(const Tuple& lhs, const Tuple& rhs) {
+        const float newx = lhs.x + rhs.x;
+        const float newy = lhs.y + rhs.y;
+        const float newz = lhs.z + rhs.z;
+        const float neww = lhs.w + rhs.w;
+
+        return {newx, newy, newz, neww};
+    }
+
+    [[nodiscard]] friend Tuple operator-(const Tuple& lhs, const Tuple& rhs) {
+        return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w};
+    }
+
+    [[nodiscard]] friend Tuple operator*(const Tuple& t, float m) {
+        return {t.x * m, t.y * m, t.z * m, t.w * m};
+    }
+
+    [[nodiscard]] friend Tuple operator/(const Tuple& t, float d) {
+        return {t.x / d, t.y / d, t.z / d, t.w / d};
+    }
+
+    [[nodiscard]] friend bool operator==(const Tuple& lhs, const Tuple& rhs) {
+        return equal(lhs.x, rhs.x) &&
+            equal(lhs.y, rhs.y) &&
+            equal(lhs.z, rhs.z) &&
+            equal(lhs.w, rhs.w);
+    }
+
+    [[nodiscard]] bool isPoint() const {
+        return equal(w, 1.0);
+    }
+
+    [[nodiscard]] bool isVector() const {
+        return equal(w, 0.0);
+    }
 
     friend std::ostream &operator<<(std::ostream &os, const Tuple& t) {
         return os << (t.isVector() ? "Vector" : "Point") << "(" << t.x << ", " << t.y << ", " << t.z << ")";
--- raytracer/main/vector.h
+++ raytracer/main/vector.h
@@ -1,7 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include <cmath>
 #include "tuple.h"
 
 struct Vector : public Tuple
@@ -15,11 +14,35 @@ struct Vector : public Tuple
 
     Vector(const Tuple& tuple) : Tuple(tuple) {}
 
-    [[nodiscard]] float magnitude() const;
-    [[nodiscard]] Vector normalize() const;
-    [[nodiscard]] float dot(const Vector& b) const;
-    [[nodiscard]] Vector cross(const Vector& b) const;
-    [[nodiscard]] Vector reflect(const Vector& normal) const;
+    [[nodiscard]] constexpr float magnitude() const {
+        // aka length
+        // Pythagoras to the rescue!
+        return std::sqrt((x*x) + (y*y) + (z*z));
+    }
+
+    [[nodiscard]] Vector normalize() const {
+        const float magnitude = this->magnitude();
+        return {x / magnitude,
+                y / magnitude,
+                z / magnitude};
+    }
+
+    [[nodiscard]] constexpr float dot(const Vector& b) const {
+        return x * b.x +
+            y * b.y +
+            z * b.z;
+    }
+
+    [[nodiscard]] Vector cross(const Vector& b) const {
+        return {y * b.z - z * b.y,
+                z * b.x - x * b.z,
+                x * b.y - y * b.x};
+    }
+
+    [[nodiscard]] Vector reflect(const Vector& normal) const {
+        return {*this - (normal * 2 * dot(normal))};
+    }
+
 };
 
 #endif // VECTOR_H
